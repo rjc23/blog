c8f7ff0bf5d841f739f420fe9abdf488b3126590
Ryan J Carmody <ryancarmody@outlook.com>
2022-07-19 12:16:06 +0930

    update fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,11 @@ import 'styles/global.css';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
